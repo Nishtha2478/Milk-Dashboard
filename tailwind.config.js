7c8b311d1c48a5f7974ff6e855082f8cba3f0081
Nishtha Patel <nishthapatel@Nishthas-MacBook-Air.local>
2025-10-05 01:22:57 -0500

    FINX

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // make sure Tailwind scans all React files
+    "./src/**/*.{js,jsx,ts,tsx}", // your source files
   ],
   theme: {
     extend: {},
