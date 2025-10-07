7385cea068d3d6bcbdc9f5de54ba7882fc2ad1c5
Nishtha2478 <nishthahp08@gmail.com>
2025-10-07 16:30:27 +0000

    Oct 7, 2025, 11:30 AM

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { supabase } from '../supabaseClient';
-import styles from '../styles';
+import '../index.css';
 
 export default function Login({ onLogin }) {
   const [email, setEmail] = useState('');
